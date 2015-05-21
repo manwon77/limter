47d5482cef6d362e2c20cb3a825ea65476f397f1
woodam <rsc3399@nate.com>
2015-05-22 00:22:02 +0900

    no message

--- js/ui/sideBar.js
+++ js/ui/sideBar.js
@@ -1,13 +1,35 @@
-define(['jquery'],function($){
+define(['jquery','easing'],function($,easing){
 	var sideBar = {
 		init : function(){
 			var btn = $(".asideBtn");
 			this.layer = $(".asdLayer");
 			this.closeBtn = $(".asd_close");
-			this.dx = this.layer.outerWidth(true);
-			console.log(this.dx)
+			this.dx = this.layer.outerWidth(true);			
+			this.oldLayer = null;
+			
 			this.layer.show();
-			//this.layer.css({"left" : 0})
+			this.layer.css({"left" : 0});
+
+			btn.on("click", $.proxy( this.slideLayer, this ) );
+			this.closeBtn.on("click", $.proxy( this.offSlide, this ) );
+		}
+		, slideLayer : function( e ){
+			var tar = $(e.currentTarget.hash);
+			if( this.oldLayer ){
+
+			}
+
+			tar.animate(
+				{"left" : this.dx-1}
+				, "800"
+				, "easeOutBack"
+				, function(){
+
+				}
+			)
+		}
+		, offSlide : function( e ){
+
 		}
 
 	}
