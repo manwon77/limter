bdf027dade0bd0676e1ade44ed8f193d62b215ac
woodam <rsc3399@nate.com>
2015-05-21 23:23:22 +0900

    상단top영역

--- js/ui/mainTop.js
+++ js/ui/mainTop.js
@@ -1,7 +1,18 @@
-define(['jquery'],function($){
+define(['jquery','easing'],function($,easing){
 	var mainTop = {
-		init : function(){
+		init : function(){			
+			var btn = $(".close");
+			this.tar = $(".topContents");
+			this.dy = this.tar.outerHeight(true);
+			btn.on("click", $.proxy(this.sladeLAyer, this ) );
 
+		}
+		, sladeLAyer : function( e ){
+			this.tar.animate(
+				{"margin-top" : this.dy*-1}
+				,"800"
+				, "easeOutBack"
+			)
 		}
 	}
 
