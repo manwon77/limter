c8828c792a41b49ee66bee455e5adf855c03e700
manwon77 <sangik20@naver.com>
2015-06-16 18:50:09 +0900

    6/16 작업 / notice_list 컨텐츠 반응형

---
+++ js/member.js
@@ -0,0 +1,56 @@
+requirejs.config({    
+    baseUrl: '../../js',    
+
+    paths: {
+        'jquery' : 'lib/jquery-1.10.2.min',
+        'easing' : 'lib/jquery.easing.1.3',
+        'pie'    : 'lib/PIE',
+
+        'shopping' : 'ui/shopping',
+        'gnb' : 'ui/gnb',
+        'rolling' : 'ui/rolling',
+        'mainTop' : 'ui/mainTop',
+        'sideBar' : 'ui/sideBar'
+
+       
+    },
+    shim: {
+        'jquery' : {
+            exports: '$'
+        }
+        , 'easing' : {
+            deps : ["jquery"],
+             exports: 'easing'
+        }
+        , 'pie' : {
+            deps : ["jquery"],
+            exports: 'pie'
+        }
+        , 'shopping' : {
+            deps : ["jquery"],
+            exports: 'shopping'   
+        }
+        , 'gnb' : {
+            deps : ["jquery"],
+            exports: 'gnb'   
+        }
+        , 'mainTop' : {
+            deps : ["jquery"],
+            exports: 'mainTop'  
+        }
+         , 'sideBar' : {
+            deps : ["jquery"],
+            exports: 'sideBar'  
+        }
+    }
+});
+
+
+
+define(['jquery','shopping','gnb', 'rolling', 'mainTop', 'sideBar'],function($,shopping, gnb, rolling, mainTop, sideBar){
+    shopping.init();
+    gnb.init();
+    rolling.init();
+    mainTop.init();
+    sideBar.init()
+})
