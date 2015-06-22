0441acaa5d158581009d95c8b37e46295721d96c
manwon77 <sangik20@naver.com>
2015-06-22 11:15:32 +0900

    utilmenu

--- js/ui/sns.js
+++ js/ui/sns.js
@@ -1,12 +1,34 @@
 define(['jquery','easing'],function($,easing){
 	var sns = {
 		init : function(){
-			
-			
-		}
 		
+			var $btn = $(".utilMenu > li > a");
+			var $sns = $(".snsWrap").css("border","1px solid red");
+
+			$btn.click(function (e) {
+				if ($btn.hasClass("active")){
+					$btn.removeClass("active");
+					$sns.clearQueue().animate(
+					{"opacity":"0","left" : 200}
+					, "800"
+					, "easeOutSine"
+					, function(){
+						$sns.hide();
+					})
+					
+				} else {
+					$(this).addClass("active");
+					$sns.clearQueue().animate(
+					{"opacity":"100","left" : -195}
+					, "800"
+					, "easeOutSine"
+					, function(){
+						$sns.show();
+					})
+				}
+			});
+		}
 	}
 	return sns;
 });
 
-	
